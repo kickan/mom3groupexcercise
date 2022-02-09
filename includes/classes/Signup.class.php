8fb5063e33465fc014211033da69411a7a5789f4
Kickan <kristina_ab@hotmail.com>
2022-02-09 09:29:47 +0100

    init project

---
+++ includes/classes/Signup.class.php
@@ -0,0 +1,32 @@
+<?php
+
+class Signup
+{
+#Properties
+private string $email;
+private array $email_list;
+
+#Method
+
+#constructor
+public function __construct(){
+
+}
+
+#Set email in object
+public function setEmail(string $email) : bool {
+
+}
+
+#save email
+public function saveEmail() : bool{
+
+}
+
+#Get list of emails
+public function getList() : array{
+
+}
+
+
+}
