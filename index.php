ad8fb3c0b1c121a0b3ac9dedd7267167abcc5176
Kickan <kristina_ab@hotmail.com>
2022-02-09 10:32:40 +0100

    save email from post

--- index.php
+++ index.php
@@ -2,10 +2,21 @@
 include("includes/classes/Signup.class.php");
 $signup = new Signup;
 
+#kolla om formuilä'r 'r postat
 
+if (isset($_POST['submit-btn'])) {
+    $email = $_POST["email"];
 
-if($signup->setEmail("hund@email.com")){;
+    if ($signup->setEmail($email)) {
+        echo "DU är anmäld!";
+        header("Location:index.php");
+    }else{
+        echo "Något gick fel";
+    }
+
 }
+
+
 
 
 
@@ -16,15 +27,17 @@ if($signup->setEmail("hund@email.com")){;
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gruppövning moment 3</title>
 </head>
+
 <body>
     <h1>Katthjälpen</h1>
-<h2>Anmälan</h2>
+    <h2>Anmälan</h2>
     <form action="index.php" method="POST">
         <label for="name">Namn: </label><br>
         <input type="text" id="name" name="name"><br>
@@ -35,15 +48,16 @@ if($signup->setEmail("hund@email.com")){;
 
     <h2>Email-lista</h2>
     <ul>
-    <?php
-    $email_list = $signup->getList();
-    foreach($email_list as $email){
-        echo "<li>$email</li>";
-    }
-    ?>
+        <?php
+        $email_list = $signup->getList();
+        foreach ($email_list as $email) {
+            echo "<li>$email</li>";
+        }
+        ?>
     </ul>
-    
+
 
 
 </body>
+
 </html>
